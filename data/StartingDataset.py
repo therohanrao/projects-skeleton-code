0311847dddeb3f998ba2c728c2f5e97c29a7d822
Brian Taylor <briantaylor@Brians-MacBook-Pro.local>
2022-02-28 18:42:38 -0800

    added data augmentation

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -3,18 +3,22 @@ from PIL import Image
 import pandas as pd
 import torchvision.transforms as transforms
 import numpy as np
+import random
 
 class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
-    def __init__(self, iseval):
+    def __init__(self, iseval, transform=None):
         # https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#
-        
 
+        self.iseval = iseval
+        self.transform = transform
+
+        
         preDF = pd.read_csv("cassava-leaf-disease-classification/train.csv")
         
-        if iseval:
+        if self.iseval:
             self.images = preDF.truncate(after = len(preDF)/5).reset_index(drop = True)
             print(self.images.info())
         else:
@@ -27,18 +31,15 @@ class StartingDataset(torch.utils.data.Dataset):
 
         im = Image.open("cassava-leaf-disease-classification/train_images/" + self.images.loc[index ,'image_id'])
         im = im.resize((224, 224))
-        trans1 = transforms.ToTensor()
-        image_tensor = trans1(im)
 
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        normalize(image_tensor)
-
-        if not is_eval:
-            #data augmentation goes here
-            #only do on test data
-            #maybe randomly select type of augmentation?
-            #maybe only augment unhealthy?
-            pass
+        augment = random.choice([0,1])
+        if augment:
+            image_tensor = self.transform(im)
+        else:
+            toTensor = transforms.ToTensor()
+            image_tensor = toTensor(im)
+            normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            normalize(image_tensor)
 
         return image_tensor, label
 
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data.StartingDataset import StartingDataset
 from networks.StartingNetwork import StartingNetwork
 from networks.StartingNetwork import StartingNetwork2
 from train_functions.starting_train import starting_train
+import torchvision.transforms as transforms
 
 
 def main():
@@ -18,9 +19,19 @@ def main():
     print("Epochs:", constants.EPOCHS)
     print("Batch size:", constants.BATCH_SIZE)
 
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(224, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    transform_val = transforms.Compose([    
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
     # Initalize dataset and model. Then train the model!
-    train_dataset = StartingDataset(0)
-    val_dataset = StartingDataset(1)
+    train_dataset = StartingDataset(0, transform = transform_train)
+    val_dataset = StartingDataset(1, transform = transform_val)
     model = StartingNetwork2()
     starting_train(
         train_dataset=train_dataset,
